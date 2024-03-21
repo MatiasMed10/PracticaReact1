b965166b26e6be8e91419bceb79d809b86be4b51
matiasmed <medinamatias63c1@gmail.com>
2024-03-21 01:20:34 -0300

    Se crearon navbar y footer y lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Footer from "./components/Footer/Footer"
+import Navbarr from "./components/Navbar/Navbar"
+import Tareas from "./components/Tareas/Tareas"
 
 function App() {
 
   return (
     <>
-      
+    <Navbarr/>
+    <Tareas/>
+    <Footer/>
     </>
   )
 }
